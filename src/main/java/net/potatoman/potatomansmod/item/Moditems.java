65d9eca2d0f40bd6036b8bfd00a24c7bc355aca4
Potatoman182 <l.w.reeves365@gmail.com>
2025-02-05 22:21:45 -0500

    Initial Commit for this project

---
+++ src/main/java/net/potatoman/potatomansmod/item/Moditems.java
@@ -0,0 +1,20 @@
+package net.potatoman.potatomansmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.potatoman.potatomansmod.Potatomansmod;
+
+public class Moditems {
+    public static  final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Potatomansmod.MOD_ID);
+
+    public static final RegistryObject<Item> VOID_BREATH_POTION = ITEMS.register("void_breath_potion",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
