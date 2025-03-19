2e48048f10693483a8590fa0293df459ca6c82a6
Potatoman182 <l.w.reeves365@gmail.com>
2025-03-19 19:42:53 -0400

    Initial Commit for this project

--- src/main/java/net/potatoman/potatomansmod/item/custom/BedrockBreakerItem.java
+++ src/main/java/net/potatoman/potatomansmod/item/custom/BedrockBreakerItem.java
@@ -7,6 +7,10 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.level.block.Block;
 
 public class BedrockBreakerItem extends Item {
 
@@ -24,10 +28,25 @@ public class BedrockBreakerItem extends Item {
 
             if (!world.isClientSide) {
                 world.setBlockAndUpdate(context.getClickedPos(), Blocks.COPPER_BLOCK.defaultBlockState());
+
+                spawnBlockBreakingParticles(world, context.getClickedPos(), blockState);
+
                 return InteractionResult.SUCCESS;
             }
         }
-
         return InteractionResult.PASS;
+    }
+    private void spawnBlockBreakingParticles(Level world, BlockPos pos, BlockState blockState) {
+
+        if (world.isClientSide) {
+
+            BlockParticleOption particleData = new BlockParticleOption(ParticleTypes.BLOCK, blockState);
+
+            world.addParticle(particleData, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 0.5, 0.5, 0.5);
+        } else {
+
+            int blockId = Block.getId(blockState);
+            world.levelEvent(2001, pos, blockId);
+        }
     }
 }
