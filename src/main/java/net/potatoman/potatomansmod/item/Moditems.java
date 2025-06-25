8a95912b74bc6f3ee38ab3cc5903f6ee766fdb39
Potatoman182 <l.w.reeves365@gmail.com>
2025-06-25 00:51:26 -0400

    Initial Commit for this project

--- src/main/java/net/potatoman/potatomansmod/item/Moditems.java
+++ src/main/java/net/potatoman/potatomansmod/item/Moditems.java
@@ -27,9 +27,9 @@ public class Moditems {
     public static final RegistryObject<Item> TUNGSTEN_ARROW = ITEMS.register("tungsten_arrow",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BEDROCK_BREAKER = ITEMS.register("bedrock_breaker",
-            () -> new BedrockBreakerItem(new Item.Properties().durability(1)));
+            () -> new BedrockBreakerItem(new Item.Properties().durability(2)));
     public static final RegistryObject<Item> RAW_VOID_HOPPER_LEG = ITEMS.register("raw_void_hopper_leg",
-            () -> new BedrockBreakerItem(new Item.Properties().food(ModFoods.Raw_Void_Hopper_Leg)));
+            () -> new Item(new Item.Properties().food(ModFoods.Raw_Void_Hopper_Leg)));
     public static final RegistryObject<Item> VOID_HOPPER_POISON_BALL = ITEMS.register("void_hopper_poison_ball",
             () -> new FuelItem(new Item.Properties(), 150));
 
--- src/main/java/net/potatoman/potatomansmod/item/custom/BedrockBreakerItem.java
+++ src/main/java/net/potatoman/potatomansmod/item/custom/BedrockBreakerItem.java
@@ -1,8 +1,11 @@
 package net.potatoman.potatomansmod.item.custom;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.Level;
@@ -12,33 +15,45 @@ import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.level.block.Block;
 import net.potatoman.potatomansmod.blocks.ModBlocks;
+import org.jetbrains.annotations.Nullable;
+import java.util.List;
 
 public class BedrockBreakerItem extends Item {
-
     public BedrockBreakerItem(Properties pProperties) {
         super(pProperties);
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext context) {
-        Level world = context.getLevel();
-        Player player = context.getPlayer();
+    public InteractionResult useOn(UseOnContext pContext) {
+        Level world = pContext.getLevel();
+        Player player1 = pContext.getPlayer();
 
-        BlockState blockState = world.getBlockState(context.getClickedPos());
+        BlockState blockState = world.getBlockState(pContext.getClickedPos());
 
         if (blockState.getBlock() == Blocks.BEDROCK) {
 
             if (!world.isClientSide) {
 
-                world.setBlockAndUpdate(context.getClickedPos(), ModBlocks.CRACKED_BEDROCK.get().defaultBlockState());
+                world.setBlockAndUpdate(pContext.getClickedPos(), ModBlocks.CRACKED_BEDROCK.get().defaultBlockState());
 
-                spawnBlockBreakingParticles(world, context.getClickedPos(), blockState);
+                spawnBlockBreakingParticles(world, pContext.getClickedPos(), blockState);
 
                 return InteractionResult.SUCCESS;
             }
         }
-        return InteractionResult.PASS;
+
+        pContext.getItemInHand().hurtAndBreak(1, pContext.getPlayer(),
+                player -> player.broadcastBreakEvent(player.getUsedItemHand()));
+
+        return InteractionResult.SUCCESS;
     }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.potatomansmod.bedrock_breaker.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
     private void spawnBlockBreakingParticles(Level world, BlockPos pos, BlockState blockState) {
         if (world.isClientSide) {
             BlockParticleOption particleData = new BlockParticleOption(ParticleTypes.BLOCK, blockState);
